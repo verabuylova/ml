6ffe106f88b6c15f3a6d5272c5cb53ae99ea7812
vera <136632671+verabuylova@users.noreply.github.com>
2024-09-02 17:55:35 +0300

    Add files via upload

---
+++ homework_practice_09_em_Buylova_Vera/metrics.py
@@ -0,0 +1,63 @@
+from typing import List, Tuple
+
+from preprocessing import LabeledAlignment
+
+
+def compute_precision(reference: List[LabeledAlignment], predicted: List[List[Tuple[int, int]]]) -> Tuple[int, int]:
+    """
+    Computes the numerator and the denominator of the precision for predicted alignments.
+    Numerator : |predicted and possible|
+    Denominator: |predicted|
+    Note that for correct metric values `sure` needs to be a subset of `possible`, but it is not the case for input data.
+
+    Args:
+        reference: list of alignments with fields `possible` and `sure`
+        predicted: list of alignments, i.e. lists of tuples (source_pos, target_pos)
+
+    Returns:
+        intersection: number of alignments that are both in predicted and possible sets, summed over all sentences
+        total_predicted: total number of predicted alignments over all sentences
+    """
+
+    intersection = sum(len(set(predicted[i]) & (set(ref.possible) | set(ref.sure))) for i, ref in enumerate(reference))
+    total_predicted = sum(len(set(pred)) for pred in predicted)
+
+    return intersection, total_predicted
+
+
+def compute_recall(reference: List[LabeledAlignment], predicted: List[List[Tuple[int, int]]]) -> Tuple[int, int]:
+    """
+    Computes the numerator and the denominator of the recall for predicted alignments.
+    Numerator : |predicted and sure|
+    Denominator: |sure|
+
+    Args:
+        reference: list of alignments with fields `possible` and `sure`
+        predicted: list of alignments, i.e. lists of tuples (source_pos, target_pos)
+
+    Returns:
+        intersection: number of alignments that are both in predicted and sure sets, summed over all sentences
+        total_predicted: total number of sure alignments over all sentences
+    """
+    intersection = sum(len(set(predicted[i]) & set(ref.sure)) for i, ref in enumerate(reference))
+    total_predicted = sum(len(set(ref.sure)) for ref in reference)
+
+    return intersection, total_predicted
+
+
+def compute_aer(reference: List[LabeledAlignment], predicted: List[List[Tuple[int, int]]]) -> float:
+    """
+    Computes the alignment error rate for predictions.
+    AER=1-(|predicted and possible|+|predicted and sure|)/(|predicted|+|sure|)
+    Please use compute_precision and compute_recall to reduce code duplication.
+
+    Args:
+        reference: list of alignments with fields `possible` and `sure`
+        predicted: list of alignments, i.e. lists of tuples (source_pos, target_pos)
+
+    Returns:
+        aer: the alignment error rate
+    """
+    inter_p, pred_p = compute_precision(reference, predicted)
+    inter_r, pred_r = compute_recall(reference, predicted)
+    return 1 - (inter_p + inter_r)/(pred_p + pred_r)
